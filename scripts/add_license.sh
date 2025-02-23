fcc967bd1f13f24df391cc69e9ebca6254547d57
Nagy András <andras.nagy@icellmobilsoft.hu>
2025-02-23 14:36:36 +0100

    gh actions test

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+reuse annotate --license="Apache-2.0" --copyright="Copyright (C) 2025 Example" ./.github/workflows/*.yml
 reuse annotate --license="Apache-2.0" --copyright="Copyright (C) 2025 Example" ./src/*
 reuse annotate --license="Apache-2.0" --copyright="Copyright (C) 2025 Example" ./scripts/*
 reuse annotate --license="Apache-2.0" --copyright="Copyright (C) 2025 Example" .pre-commit-config.yaml README.md
